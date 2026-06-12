ad7037ac361498cc322c2860a2632acafeb5f0b7
Wallyson <wallysonm51@gmail.com>
2026-06-12 18:27:14 -0300

    Enhance contar function with zero value checks
    Added a check for zero values in the step and end number.

--- script.js
+++ script.js
@@ -8,8 +8,11 @@ resultado.innerHTML = "";
 if ((inicio.value == "" || fim.value == "" || passo.value == "")) {
     resultado.innerHTML = `Impossível contar!`
 }
-else{
-for (let i = Number(inicio.value); i <= Number(fim.value); i += Number(passo.value)) {
-    resultado.innerHTML += i + " ";}}
+else if (Number(passo.value) == 0 || Number(fim.value) == 0) {
+    resultado.innerHTML = `Impossível contar com numeros zero!`
 }
-  
+else {
+    for (let i = Number(inicio.value); i <= Number(fim.value); i += Number(passo.value)) {
+        resultado.innerHTML += i + " ";
+    }
+}}
