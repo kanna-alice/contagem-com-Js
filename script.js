819d1dc175174694911f1e48d7e243a415f54346
Wallyson <wallysonm51@gmail.com>
2026-06-13 14:18:16 -0300

    Improve contar function readability and structure
    Refactor contar function with additional comments for clarity.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function contar() {
+    // Obtendo os elementos de entrada e resultado
     let inicio = document.getElementById('inicio')
     let fim = document.getElementById('fim')
     let passo = document.getElementById('passo')
@@ -9,22 +10,27 @@ function contar() {
     if (inicio.value === "" || fim.value === "" || passo.value == "") {
         resultado.innerHTML = `Impossível contar!`
     } else {
+        // Convertendo os valores para números
         let c = Number(inicio.value);
         let f = Number(fim.value);
         let p = Number(passo.value);
-
+        // Verificando se o passo é zero
         if (p === 0) {
             resultado.innerHTML = `Impossível contar com zero!`
-        } else {
+        } 
+        // Verificando se o passo é negativo
+        else {
             if (c < f) {
                 for (let i = c; i <= f; i += p) {
                     resultado.innerHTML += i + " ";
                 }
+                // Verificando se o passo é positivo
             } else {
                 for (let i = c; i >= f; i -= p) {
                     resultado.innerHTML += i + " ";
                 }
             }
+            // Verificando se o passo é zero para evitar um loop infinito
             if (p === 0){
                 p = 1
                 for (let i = c; i <= f; i += p) {
