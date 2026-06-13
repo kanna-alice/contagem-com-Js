6fb2e1525dd62de25abd07ce712fd23dcf603ede
Wallyson <wallysonm51@gmail.com>
2026-06-13 13:51:10 -0300

    Refactor contar function to handle zero step cases

--- script.js
+++ script.js
@@ -8,11 +8,22 @@ resultado.innerHTML = "";
 if ((inicio.value == "" || fim.value == "" || passo.value == "")) {
     resultado.innerHTML = `Impossível contar!`
 }
-else if (Number(passo.value) == 0 || Number(fim.value) == 0) {
-    resultado.innerHTML = `Impossível contar com numeros zero!`
+
+else if (Number(passo.value) == 0 || Number(inicio.value) == 0 || Number(fim.value) == 0) {
+    resultado.innerHTML = `Impossível contar com  zero!`
+}
+else if (Number(passo.value) === 0) { 
+    for (let i = Number(inicio.value); i <= Number(fim.value); i += 1) {
+    alert("Passo inválido! iremos contar normalmente.")    
+    resultado.innerHTML += i + " ";
+    }
 }
 else {
-    for (let i = Number(inicio.value); i <= Number(fim.value); i += Number(passo.value)) {
+    let c = Number(inicio.value);
+    let p = Number(passo.value);
+    let f = Number(fim.value);
+    for (let i = Number(inicio.value); i <= f; i += p) {
         resultado.innerHTML += i + " ";
     }
+    resultado.innerHTML += "FIM!";
 }}
