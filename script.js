891c83ea36e16033651871081b1029b42a9cc7aa
Wallyson <wallysonm51@gmail.com>
2026-06-13 14:16:19 -0300

    otimização

--- script.js
+++ script.js
@@ -1,29 +1,35 @@
 function contar() {
-let inicio = document.getElementById('inicio')
-let fim = document.getElementById('fim')
-let passo = document.getElementById('passo')
-let resultado = document.getElementById('resultado')
-let limite = Number(fim.value)
-resultado.innerHTML = "";
-if ((inicio.value == "" || fim.value == "" || passo.value == "")) {
-    resultado.innerHTML = `Impossível contar!`
-}
+    let inicio = document.getElementById('inicio')
+    let fim = document.getElementById('fim')
+    let passo = document.getElementById('passo')
+    let resultado = document.getElementById('resultado')
 
-else if (Number(passo.value) == 0 || Number(inicio.value) == 0 || Number(fim.value) == 0) {
-    resultado.innerHTML = `Impossível contar com  zero!`
-}
-else if (Number(passo.value) === 0) { 
-    for (let i = Number(inicio.value); i <= Number(fim.value); i += 1) {
-    alert("Passo inválido! iremos contar normalmente.")    
-    resultado.innerHTML += i + " ";
-    }
-}
-else {
-    let c = Number(inicio.value);
-    let p = Number(passo.value);
-    let f = Number(fim.value);
-    for (let i = Number(inicio.value); i <= f; i += p) {
-        resultado.innerHTML += i + " ";
-    }
-    resultado.innerHTML += "FIM!";
-}}
+    resultado.innerHTML = "";
+    
+    if (inicio.value === "" || fim.value === "" || passo.value == "") {
+        resultado.innerHTML = `Impossível contar!`
+    } else {
+        let c = Number(inicio.value);
+        let f = Number(fim.value);
+        let p = Number(passo.value);
+
+        if (p === 0) {
+            resultado.innerHTML = `Impossível contar com zero!`
+        } else {
+            if (c < f) {
+                for (let i = c; i <= f; i += p) {
+                    resultado.innerHTML += i + " ";
+                }
+            } else {
+                for (let i = c; i >= f; i -= p) {
+                    resultado.innerHTML += i + " ";
+                }
+            }
+            if (p === 0){
+                p = 1
+                for (let i = c; i <= f; i += p) {
+                    resultado.innerHTML += i + " ";
+                }
+            }
+            resultado.innerHTML += "FIM!";
+        }}}
